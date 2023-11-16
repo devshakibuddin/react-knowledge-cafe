5328719c8497c1fd84fc684bc168b8e9708a29ad
devshakibuddin <112326077+devshakibuddin@users.noreply.github.com>
2023-11-17 01:05:43 +0600

    creat fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import SideCard from './components/SideCard/SideCard';
 
 
 function App() {
@@ -11,6 +12,14 @@ function App() {
   return (
     <>
       <Header></Header>
+      <div className='main row'>
+        <div className="home-container col-md-8 card">
+          <Home></Home>
+        </div>
+        <div className="sideCard-container col-md-4 card">
+          <SideCard></SideCard>
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/SideCard/SideCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideCard = () => {
+    return (
+        <div>
+            <h2>Side card here </h2>
+        </div>
+    );
+};
+
+export default SideCard;
